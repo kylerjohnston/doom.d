320998e9fe200ed679fcd8030304721dbea1fd71
Kyle Johnston <kyle.johnston@vetrofibermap.com>
2021-03-03 09:58:48 -0500

    remove salt-mode

--- config.el
+++ config.el
@@ -60,10 +60,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-;; salt
-(add-hook 'salt-mode-hook
-          (lambda()
-            (flyspell-mode 1)))
 
 (after! lsp-clients
   (lsp-register-client
